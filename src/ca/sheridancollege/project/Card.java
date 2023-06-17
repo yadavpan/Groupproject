6489dd08dd52c06b2711b2bd1f550e6e5fd4ade5
yadavpan <yadavpan@sheridancollege.ca>
2023-06-16 23:52:07 -0400

    This is my first commit for Group project 1

---
+++ src/ca/sheridancollege/project/Card.java
@@ -0,0 +1,25 @@
+/**
+ * THIS CODE IS CREATED BY PANKAJ YADAV
+ * DATE OF CREATION 16-06-2023
+ * 
+ */
+package ca.sheridancollege.project;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author dancye
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
